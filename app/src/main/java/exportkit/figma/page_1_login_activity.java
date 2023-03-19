af26aaeebc60c9dfc6404dc8d806f71cfe1c277a
gtsoo <gtsooju@gmail.com>
2023-03-19 21:23:14 +0900

    Initial commit

---
+++ app/src/main/java/exportkit/figma/page_1_login_activity.java
@@ -0,0 +1,95 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		page_1_login
+	 *	@date 		Saturday 18th of March 2023 02:08:58 PM
+	 *	@title 		Mentors
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class page_1_login_activity extends Activity {
+
+	
+	private View _bg__page_1_login;
+	private ImageView rectangle_4204;
+	private ImageView username;
+	private ImageView password;
+	private ImageView remember_me;
+	private ImageView forgot_password_;
+	private ImageView register_now;
+	private ImageView log_in;
+	private ImageView rectangle_4202__stroke_;
+	private ImageView rectangle_4203__stroke_;
+	private ImageView or_login_with;
+	private ImageView vector_49__stroke_;
+	private ImageView vector_50__stroke_;
+	private ImageView rectangle_4205;
+	private ImageView vector_70__stroke_;
+	private ImageView log_in_ek1;
+	private View facebook;
+	private ImageView vector;
+	private View google;
+	private ImageView vector_ek1;
+	private ImageView vector_ek2;
+	private ImageView vector_ek3;
+	private ImageView vector_ek4;
+	private View apple_id;
+	private ImageView vector_ek5;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.page_1_login);
+
+		
+		_bg__page_1_login = (View) findViewById(R.id._bg__page_1_login);
+		rectangle_4204 = (ImageView) findViewById(R.id.rectangle_4204);
+		username = (ImageView) findViewById(R.id.username);
+		password = (ImageView) findViewById(R.id.password);
+		remember_me = (ImageView) findViewById(R.id.remember_me);
+		forgot_password_ = (ImageView) findViewById(R.id.forgot_password_);
+		register_now = (ImageView) findViewById(R.id.register_now);
+		log_in = (ImageView) findViewById(R.id.log_in);
+		rectangle_4202__stroke_ = (ImageView) findViewById(R.id.rectangle_4202__stroke_);
+		rectangle_4203__stroke_ = (ImageView) findViewById(R.id.rectangle_4203__stroke_);
+		or_login_with = (ImageView) findViewById(R.id.or_login_with);
+		vector_49__stroke_ = (ImageView) findViewById(R.id.vector_49__stroke_);
+		vector_50__stroke_ = (ImageView) findViewById(R.id.vector_50__stroke_);
+		rectangle_4205 = (ImageView) findViewById(R.id.rectangle_4205);
+		vector_70__stroke_ = (ImageView) findViewById(R.id.vector_70__stroke_);
+		log_in_ek1 = (ImageView) findViewById(R.id.log_in_ek1);
+		facebook = (View) findViewById(R.id.facebook);
+		vector = (ImageView) findViewById(R.id.vector);
+		google = (View) findViewById(R.id.google);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		apple_id = (View) findViewById(R.id.apple_id);
+		vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
